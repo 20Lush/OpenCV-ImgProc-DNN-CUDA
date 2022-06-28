95659369f99d6e4f85858454006358ba0c9e7d40
20Lush <zachfsx83@gmail.com>
2022-06-28 13:49:29 -0700

    Added getCenterSquare func + GPU crop handling

--- main.cpp
+++ main.cpp
@@ -1,66 +1,65 @@
 #include "misc-functions.hpp"
 
-
-// int main(int, char**) {
-
-//     string BASE_PATH = "C:/Users/zachf/Documents/.Computer Vision/opencv-gpu-test/";
-//     string valorant = "valorant_model/";
-
-//     // VideoCapture cap;
-//     // cap.open(0);
-//     // cap.set(CAP_PROP_FRAME_WIDTH, 1920);
-//     // cap.set(CAP_PROP_FRAME_HEIGHT, 1080);
-//     // cap.set(CAP_PROP_FPS, 30);
-//     // cap.set(CAP_PROP_AUTO_EXPOSURE, false);
-
-//     vector<string> CLASS_NAMES;
-//     ifstream ifs(string(BASE_PATH + "coco.names").c_str());
-//     string line;
-
-//     while(getline(ifs, line))
-//         CLASS_NAMES.push_back(line);
-
-//     auto net = readNetFromDarknet(BASE_PATH + "yolov3-tiny.cfg", BASE_PATH + "yolov3-tiny.weights");
-
-//     net.setPreferableBackend(DNN_BACKEND_CUDA);
-//     net.setPreferableTarget(DNN_TARGET_CUDA);
-
-//     Mat frame = imread(BASE_PATH + "testphoto.jpg");
-
-//     for(;;){
-
-//         // cap >> frame;
-
-//         Mat blob = blobFromImage(frame, 1/255.0, Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(0,0,0), true, false);
-
-//         net.setInput(blob);
-
-//         vector<Mat> outputs;
-//         net.forward(outputs, getOutputNames(net));
-
-//         postprocess(frame, outputs, CLASS_NAMES);
-
-//         imshow("frame", frame);
-
-//         if(waitKey(1) == 'q')
-//             break;
-
-//     }
-
-//     // cap.release();
-//     destroyAllWindows();
-//     return 0;
-
-// }
-
-int main(){
+constexpr int IMG_WIDTH = 2560;
+constexpr int IMG_HEIGHT = 1440;
 
 
-    Mat frame = imread("C:/Users/zachf/Documents/.Computer Vision/opencv-gpu-test/testphoto.jpg");
+int main(int, char**) {
 
-    imshow("frame", frame);
+    string BASE_PATH = "C:/Users/zachf/Documents/.Computer Vision/opencv-gpu-test/";
+    string valorant = "valorant_model/";
 
-    waitKey(0);
+    VideoCapture cap;
+    cap.open(0);
+    cap.set(CAP_PROP_FRAME_WIDTH, IMG_WIDTH);
+    cap.set(CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT);
+    cap.set(CAP_PROP_FPS, 60);
+    cap.set(CAP_PROP_AUTO_EXPOSURE, false);
+
+    vector<string> CLASS_NAMES;
+    ifstream ifs(string(BASE_PATH + valorant + "coco-dataset.labels").c_str());
+    string line;
+
+    while(getline(ifs, line))
+        CLASS_NAMES.push_back(line);
+
+    auto net = readNetFromDarknet(BASE_PATH + valorant + "yolov4-tiny.cfg", BASE_PATH + valorant + "yolov4-tiny.weights");
+
+    net.setPreferableBackend(DNN_BACKEND_CUDA);
+    net.setPreferableTarget(DNN_TARGET_CUDA);
+
+    Mat frame;
+
+    for(;;){
+
+        cap >> frame;
+
+        cuda::GpuMat gpu_fullImage;
+        gpu_fullImage.upload(frame);
+
+        cuda::GpuMat gpu_croppedImage = gpu_fullImage(getCenterSquare(IMG_WIDTH, IMG_HEIGHT, INPUT_WIDTH)); // more for offsetting system resources than any percieved performance gained. Its cool and I wanted to do it ok
+        
+        Mat croppedImage; 
+        gpu_croppedImage.download(croppedImage);
+        
+        Mat blob = blobFromImage(croppedImage, 1/255.0, Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(0,0,0), true, false);
+        net.setInput(blob);
+
+        vector<Mat> outputs;
+        net.forward(outputs, getOutputNames(net));
+
+        postProcess(croppedImage, outputs, CLASS_NAMES);
+
+        imshow("frame", croppedImage);
+
+        if(waitKey(1) == 'q')
+            break;
+
+    }
+
+    cap.release();
     destroyAllWindows();
     return 0;
+
 }
+
