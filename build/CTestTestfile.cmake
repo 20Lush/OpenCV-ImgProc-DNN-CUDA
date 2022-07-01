df2a44f9982d22767c9a618e2cd11aa9df028aef
20Lush <zachfsx83@gmail.com>
2022-07-01 00:53:40 -0700

    fixed version control

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zachf/Documents/.Computer Vision/opencv-gpu-test
-# Build directory: C:/Users/zachf/Documents/.Computer Vision/opencv-gpu-test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
