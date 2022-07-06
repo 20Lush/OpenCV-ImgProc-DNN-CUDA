0df1943b3920dbd2e901b5ff501c8b14477fa4f4
20Lush <zachfsx83@gmail.com>
2022-07-06 12:28:39 -0700

    target pass by reference added + some cleanup

--- Analysis.hpp
+++ Analysis.hpp
@@ -11,15 +11,14 @@ using namespace dnn;
 
 
 
-// 7/1/2022 --------------------------
-// -> need to program the closest-detection algo
-// -----. store the index of the closest detection, run dist as pythagorean theorem btw center and box's center.
+// 7/3/2022 --------------------------
+// -> Clean this up
+
 
 class Analysis {
     public:
         const float CONFIDENCE_THRESHOLD = 0.5f; //should probably have this initialized somewhere else for customizeability
         const float NMS_THRESHOLD = 0.3f;
-
         int INPUT_WIDTH = 416;
         int INPUT_HEIGHT = 416;
         int IMG_WIDTH = 2560;
@@ -29,143 +28,156 @@ class Analysis {
         Point closest, current;
         Point center{ (int)(INPUT_WIDTH*0.5), (int)(INPUT_HEIGHT*0.5)};
 
-        vector<String> getOutputNames(const Net& net){
-            static vector<String> names;
-            if(names.empty()){
+        vector<String> getOutputNames(const Net& net);
+        Rect getCenterSquare(int screen_width, int screen_height, int length);
+        Point rectCenter(Rect box);
 
-                vector<int> output_layers = net.getUnconnectedOutLayers(); // gets indices of output layers -> layers without unconnected outputs
-                vector<String> layers_names = net.getLayerNames(); // gets names of all layers in net
+        void postProcess(Mat& frame, const vector<Mat>& outputs, vector<string>& classes, Point* target);
+        void drawDetectionCount(Mat& frame);
 
-                names.resize(output_layers.size()); // compile all found names of output layers into string vector
-                for(int i = 0; i < output_layers.size(); i++)
-                    names[i] = layers_names[output_layers[i] - 1];
-            }
-            return names;
-        }
 
-        Rect getCenterSquare(int screen_width, int screen_height, int length){ //returns a Rect equivalent to the (x,y) & (w,h) coords representing the center of the screen. Use to crop out everything but center.
-
-            int sqr_offset = (int)(length * 0.5); //at center coord, add to (Y) to get opencv rect y coord. subtract from (X) to get opencv rect x coord.
-            int sqr_centerX = (int)(screen_width * 0.5);
-            int sqr_centerY = (int)(screen_height * 0.5);
-            
-            return Rect(sqr_centerX - sqr_offset, sqr_centerY - sqr_offset, length, length);
-
-        }
-
-        void postProcess(Mat& frame, const vector<Mat>& outputs, vector<string>& classes){
-
-            vector<int> class_IDs;
-            vector<float> confidences;
-            vector<Rect> boxes;
-
-            for(int i = 0; i < outputs.size(); i++){ // taking each output from the net and doing a cumbersome O(n^2) walk over them to refine them to an intuitive format
-
-                float* data_ptr = (float*)outputs[i].data; // points to a row(detection w/ associated info)
-
-                for(int j = 0; j < outputs[i].rows; j++, data_ptr += outputs[i].cols){ // walk through the row at a given col
-
-                    Mat scores = outputs[i].row(j).colRange(5, outputs[i].cols); // locates class confidence values for each detection matrix (all values are > cols[5])
-                    Point classIDPoint;                                          // to explain better: col[0->4] are all static properties of the detection like screen coordinates ect
-                    double confidence;
-
-                    minMaxLoc(scores, 0, &confidence, 0, &classIDPoint); // finds maximum confidence core with associated class ID in detection matrix
-
-                    if(confidence > CONFIDENCE_THRESHOLD){ // extrapolating useful properties from each net detection
-
-                        int centerX = (int)(data_ptr[0] * frame.cols); // detection's x val * frame's col pixel count
-                        int centerY = (int)(data_ptr[1] * frame.rows); // detection's y val * frame's row pixel count
-                        int width = (int)(data_ptr[2] * frame.cols);
-                        int height = (int)(data_ptr[3] * frame.rows); 
-                        int left = (int)(centerX - (width * 0.5)); //a very mid and unsatisfying way to find these values
-                        int top = (int)(centerY - (height * 0.5));
-
-                        //the goal outputs of this block.
-                        class_IDs.push_back(classIDPoint.x);
-                        confidences.push_back((float) confidence);
-                        boxes.push_back(Rect(left, top, width, height));
-
-                    }
-                }
-            }
-
-            //true post-processing where non-max supression eliminates overlaps, modulated with NMS_THRESHOLD
-            vector<int> indices;
-            NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD, indices); //suppress non-max conf value bounding boxes and eliminate overlapping boxes
-
-            closest = center; //center of FOV
-
-            // iter through the detections and do what you need to do to them. current det index is idx
-            for(size_t i = 0; i < indices.size(); i++){
-                int idx = indices[i];
-                Rect box = boxes[idx];
-
-                current = rectangleCenter(box);
-                if(findAbsoluteDistanceFromCenter(current) < findAbsoluteDistanceFromCenter(closest)){
-                    closest = current;
-                    closest_idx = idx; 
-                }
-
-                //compute findAbsoluteDistanceFromCenter into mouse moves and send through serial
-
-                drawBoundingBox(class_IDs[idx], confidences[idx], box.x, box.y, box.x + box.width, box.y + box.height, frame, classes);
-
-                drawCenterDot(frame, boxes[closest_idx]);
-                drawCorrectionVector(frame, boxes[closest_idx]);
-
-                DET_COUNT++;
-            }
-
-        }
-
-        void drawDetectionCount(Mat& frame){ //Draws a counter on the top left corner of the image
-
-            String count = to_string(DET_COUNT);
-            int drawline;
-            Size textSize = getTextSize(count, FONT_HERSHEY_SIMPLEX, 0.75, 1, &drawline);
-            //top = max(top, textSize.height);
-            rectangle(frame, Point(0,5), Point((int)(round(1.75*textSize.width)), (int)(round(1.75*textSize.height))),  Scalar(255,255,255), FILLED);
-
-            putText(frame, count, Point(5,25), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0),1);
-        }
 
     private:
-        Point rectangleCenter(Rect box){ //generic center [might evolve to upper centroid later]
-            return Point( (int)( (box.x + (box.width*0.5)) ), (int)( (box.y + (box.height * 0.5)) ) ); 
-        }
-
-        double findAbsoluteDistanceFromCenter(Point pt){ //find the absolute distance from pt1 to pt2
-            return double(sqrt( pow((pt.x - center.x),2) + pow((pt.y - center.y),2) ));
-        }
-        void drawBoundingBox(int classID, float confidence, int left, int top, int right, int bottom, Mat& frame, vector<string>& classes){
-
-            // draws a rectangle using the extrapolated coords from postProcess
-            rectangle(frame, Point(left,top), Point(right, bottom), Scalar(255,178,50), 3);
-
-            // literal black magic to get text to display nicely with a filled backdrop
-            string label = format("%.2f", confidence);
-            if(!classes.empty()){
-
-                CV_Assert( classID < (int)classes.size());
-                label = classes[classID] + ":" + label;
-            }
-            int drawline;
-            Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &drawline);
-            top = max(top, labelSize.height);
-            rectangle(frame, Point(left,(int)(top - round(1.5*labelSize.height))), Point((int)(left+round(1.5*labelSize.width)), top+drawline),  Scalar(255,255,255), FILLED);
-            putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0), 1);
-        }
-
-        void drawCenterDot(Mat& image, Rect box){
-            Point m_center = rectangleCenter(box);
-            circle(image, rectangleCenter(box), 5, Scalar(50,178,255), FILLED);
-        }
-
-        void drawCorrectionVector(Mat& image, Rect box){
-            Point detection_center = rectangleCenter(box);
-            line(image, detection_center, center, Scalar(50,178,255));
-
-        }
+        double findAbsoluteDistanceFromCenter(Point pt);
+        
+        void drawBoundingBox(int classID, float confidence, int left, int top, int right, int bottom, Mat& frame, vector<string>& classes);
+        void drawCenterDot(Mat& image, Rect box);
+        void drawCorrectionVector(Mat& image, Rect box);
 
 };
 
+vector<String> Analysis::getOutputNames(const Net& net){
+    static vector<String> names;
+    if(names.empty()){
+
+        vector<int> output_layers = net.getUnconnectedOutLayers(); // gets indices of output layers -> layers without unconnected outputs
+        vector<String> layers_names = net.getLayerNames(); // gets names of all layers in net
+
+        names.resize(output_layers.size()); // compile all found names of output layers into string vector
+        for(int i = 0; i < output_layers.size(); i++)
+            names[i] = layers_names[output_layers[i] - 1];
+    }
+    return names;
+}
+
+Rect Analysis::getCenterSquare(int screen_width, int screen_height, int length){ //returns a Rect equivalent to the (x,y) & (w,h) coords representing the center of the screen. Use to crop out everything but center.
+
+    int sqr_offset = (int)(length * 0.5); //at center coord, add to (Y) to get opencv rect y coord. subtract from (X) to get opencv rect x coord.
+    int sqr_centerX = (int)(screen_width * 0.5);
+    int sqr_centerY = (int)(screen_height * 0.5);
+    
+    return Rect(sqr_centerX - sqr_offset, sqr_centerY - sqr_offset, length, length);
+
+}
+
+void Analysis::postProcess(Mat& frame, const vector<Mat>& outputs, vector<string>& classes, Point* target){
+
+    vector<int> class_IDs;
+    vector<float> confidences;
+    vector<Rect> boxes;
+
+    for(int i = 0; i < outputs.size(); i++){ // taking each output from the net and doing a cumbersome O(n^2) walk over them to refine them to an intuitive format
+
+        float* data_ptr = (float*)outputs[i].data; // points to a row(detection w/ associated info)
+
+        for(int j = 0; j < outputs[i].rows; j++, data_ptr += outputs[i].cols){ // walk through the row at a given col
+
+            Mat scores = outputs[i].row(j).colRange(5, outputs[i].cols); // locates class confidence values for each detection matrix (all values are > cols[5])
+            Point classIDPoint;                                          // to explain better: col[0->4] are all static properties of the detection like screen coordinates ect
+            double confidence;
+
+            minMaxLoc(scores, 0, &confidence, 0, &classIDPoint); // finds maximum confidence core with associated class ID in detection matrix
+
+            if(confidence > CONFIDENCE_THRESHOLD){ // extrapolating useful properties from each net detection
+
+                int centerX = (int)(data_ptr[0] * frame.cols); // detection's x val * frame's col pixel count
+                int centerY = (int)(data_ptr[1] * frame.rows); // detection's y val * frame's row pixel count
+                int width = (int)(data_ptr[2] * frame.cols);
+                int height = (int)(data_ptr[3] * frame.rows); 
+                int left = (int)(centerX - (width * 0.5)); //a very mid and unsatisfying way to find these values
+                int top = (int)(centerY - (height * 0.5));
+
+                //the goal outputs of this block.
+                class_IDs.push_back(classIDPoint.x);
+                confidences.push_back((float) confidence);
+                boxes.push_back(Rect(left, top, width, height));
+
+            }
+        }
+    }
+
+    //true post-processing where non-max supression eliminates overlaps, modulated with NMS_THRESHOLD
+    vector<int> indices;
+    NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD, indices); //suppress non-max conf value bounding boxes and eliminate overlapping boxes
+
+    closest = center; //center of FOV
+
+    // iter through the detections and do what you need to do to them. current det index is idx
+    for(size_t i = 0; i < indices.size(); i++){
+        int idx = indices[i];
+        Rect box = boxes[idx];
+
+        current = rectCenter(box);
+        if(findAbsoluteDistanceFromCenter(current) < findAbsoluteDistanceFromCenter(closest)){
+            closest = current;
+            closest_idx = idx; 
+        }
+
+      
+        drawBoundingBox(class_IDs[idx], confidences[idx], box.x, box.y, box.x + box.width, box.y + box.height, frame, classes);
+
+        //drawCenterDot(frame, boxes[closest_idx]);
+        drawCorrectionVector(frame, boxes[closest_idx]);
+        
+        
+        DET_COUNT++;
+    }
+    
+}
+
+void Analysis::drawDetectionCount(Mat& frame){ //Draws a counter on the top left corner of the image
+
+    String count = to_string(DET_COUNT);
+    int drawline;
+    Size textSize = getTextSize(count, FONT_HERSHEY_SIMPLEX, 0.75, 1, &drawline);
+    //top = max(top, textSize.height);
+    rectangle(frame, Point(0,5), Point((int)(round(1.75*textSize.width)), (int)(round(1.75*textSize.height))),  Scalar(255,255,255), FILLED);
+
+    putText(frame, count, Point(5,25), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0),1);
+}
+
+Point Analysis::rectCenter(Rect box){ //generic center [might evolve to upper centroid later]
+    return Point( (int)( (box.x + (box.width*0.5)) ), (int)( (box.y + (box.height * 0.5)) ) ); 
+}
+
+double Analysis::findAbsoluteDistanceFromCenter(Point pt){ //find the absolute distance from pt1 to pt2
+    return double(sqrt( pow((pt.x - center.x),2) + pow((pt.y - center.y),2) ));
+}
+
+void Analysis::drawBoundingBox(int classID, float confidence, int left, int top, int right, int bottom, Mat& frame, vector<string>& classes){
+
+    // draws a rectangle using the extrapolated coords from postProcess
+    rectangle(frame, Point(left,top), Point(right, bottom), Scalar(255,178,50), 3);
+
+    // literal black magic to get text to display nicely with a filled backdrop
+    string label = format("%.2f", confidence);
+    if(!classes.empty()){
+
+        CV_Assert( classID < (int)classes.size());
+        label = classes[classID] + ":" + label;
+    }
+    int drawline;
+    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &drawline);
+    top = max(top, labelSize.height);
+    rectangle(frame, Point(left,(int)(top - round(1.5*labelSize.height))), Point((int)(left+round(1.5*labelSize.width)), top+drawline),  Scalar(255,255,255), FILLED);
+    putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0), 1);
+}
+
+void Analysis::drawCenterDot(Mat& image, Rect box){
+    circle(image, rectCenter(box), 5, Scalar(50,178,255), FILLED);
+}
+
+void Analysis::drawCorrectionVector(Mat& image, Rect box){
+    Point detection_center = rectCenter(box);
+    line(image, detection_center, center, Scalar(50,178,255));
+}
--- main.cpp
+++ main.cpp
@@ -5,25 +5,52 @@ int main(int, char**) {
 
     Analysis analysis;
 
+    #pragma region video_capture_with_properties // cv::VideoCapture setup. [] OBJ: "cap" []
+    // +----------------------------------------------------+
+
     cv::VideoCapture cap;
+
     cap.open(0);
+
     cap.set(cv::CAP_PROP_FRAME_WIDTH, analysis.IMG_WIDTH); //could definitally find all of these esoteric values through hwnd
+
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, analysis.IMG_HEIGHT);
+
     cap.set(cv::CAP_PROP_FPS, 60);
+
     cap.set(cv::CAP_PROP_AUTO_EXPOSURE, false);
 
+    // +----------------------------------------------------+
+    #pragma endregion video_capture_with_properties
+
+    #pragma region file_pathing // file paths stored as strings. 
+    // +----------------------------------------------------+
+
     string BASE_PATH = "C:/Users/zachf/Documents/.Computer Vision/opencv-gpu-test/"; //need to eventually find a more portable way to do this (user prompt maybe?)
+
     string valorant = "non_descript_game_model/"; //i hate file systems
 
-    vector<string> CLASS_NAMES;
     ifstream ifs(string(BASE_PATH + valorant + "coco-dataset.labels").c_str());
+
+    // +----------------------------------------------------+
+    #pragma endregion file_pathing
+
+    vector<string> CLASS_NAMES;
     string line;
     while(getline(ifs, line))
         CLASS_NAMES.push_back(line);
 
+    #pragma region dnn_spinup
+    // +----------------------------------------------------+
+
     auto net = cv::dnn::readNetFromDarknet(BASE_PATH + valorant + "yolov4-tiny.cfg", BASE_PATH + valorant + "yolov4-tiny.weights");
+
     net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+
     net.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
+
+    // +----------------------------------------------------+
+    #pragma endregion dnn_spinup
 
     cv::Mat frame;
     for(;;){
@@ -31,37 +58,42 @@ int main(int, char**) {
         cap >> frame;
         analysis.DET_COUNT = 0;
 
+        #pragma region image_mutation 
+        // +----------------------------------------------------+
+
         cv::cuda::GpuMat gpu_fullImage;
+
         gpu_fullImage.upload(frame);
 
         cv::cuda::GpuMat gpu_croppedImage = gpu_fullImage(analysis.getCenterSquare(analysis.IMG_WIDTH, analysis.IMG_HEIGHT, analysis.INPUT_WIDTH)); // more for offsetting system resources than any percieved performance gained. Its cool and I wanted to do it ok
         
         cv::Mat croppedImage; 
+
         gpu_croppedImage.download(croppedImage);
         
         cv::Mat blob = blobFromImage(croppedImage, 1/255.0, cv::Size(analysis.INPUT_WIDTH, analysis.INPUT_HEIGHT), cv::Scalar(0,0,0), true, false);
+
         net.setInput(blob);
 
+        // +----------------------------------------------------+
+        #pragma endregion image_mutation 
+
+        #pragma region neural_net_impl
+        // +----------------------------------------------------+
+
         vector<cv::Mat> outputs;
+
         net.forward(outputs, analysis.getOutputNames(net));
 
-        analysis.postProcess(croppedImage, outputs, CLASS_NAMES);
+        // +----------------------------------------------------+
+        #pragma endregion neural_net_impl
 
         //instantiate a Rect for which postProcess will transfer the box[closest_idx] to
-        //have a "virtual mouse pointer" that is a little red circle, for which the Point(x,y) will be mutated
-        //take the dx and dy between the fake mouse and the center of the Rect
-        //add the dx and dy to the center coords of the fake mouse circle, with both of those divided by a step value
-        //if there are no detections, return mouse to center
-        //might need some sort of way to dump whatever is in the buffer Rect here that stores the closest box, can be returned from postProcess
-        //or can actually just call on DET_COUNT
-
+        Point target; 
+        analysis.postProcess(croppedImage, outputs, CLASS_NAMES, &target); // -> push box[closest_dx] into rect
         analysis.drawDetectionCount(croppedImage);
 
-
-
         imshow("frame", croppedImage);
-        if(cv::waitKey(1) == 'q')
-            break;
 
     }
 
